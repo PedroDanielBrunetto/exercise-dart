0f489f7e7c7a44803ec3fc0ca9a12b1834811d5d
pedrodanielbrunetto <pedrodanielbm@hotmail.com>
2023-07-31 17:24:53 -0300

    first-commit

---
+++ ex2.dart
@@ -0,0 +1,7 @@
+main(){
+  String etec = "Etec Dra Ruth Cardoso - Curso de Desenvolvimento de Sistemas";
+  String nome = "Pedro Daniel";
+  int yo = 18;
+
+  print("Olá, me chamo $nome, tenho $yo e sou aluno da $etec");
+}
---
+++ ex3.dart
@@ -0,0 +1,11 @@
+main(){
+  double mat = 8.7;
+  double bio = 9.2;
+  double hist = 8;
+  double filo = 10;
+  double biblia = 10;
+
+  double media = (mat + bio + hist + filo + biblia) / 5;
+
+  print("A média do aluno é: $media");
+}
